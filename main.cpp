d7f31c953dd3adc16babbe31b1af049b35b4d868
Svein Kåre <sveiks@stud.ntnu.no>
2026-04-09 12:34:56 +0200

    Implemented triangle function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include "landscapes.cpp"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+  TriangleLandscape<16> test(1, 4);
+  bitset<16> t(0);
+  for (int i = 0; i < 16; i++) {
+    cout << test.fitness(t).first << endl;
+    t.set(i, true);
+  }
 
   return 0;
 }
